be8f8f003275a27e981c2df7e43901aab3391f2e
Данияр Ермагамбетов <karioko767.se87@gmail.com>
2024-02-14 09:01:48 +0600

    Домашнее задание выполнено

--- Daniyar_ASP/Program.cs
+++ Daniyar_ASP/Program.cs
@@ -1,21 +1,50 @@
+using System.IO;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using static System.Net.Mime.MediaTypeNames;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.Run(async (context) =>
 {
-    context.Response.ContentType = "text/html; charset=utf-8";
+    var path = context.Request.Path;
+    var response = context.Response;
 
-    // если обращение идет по адресу "/postuser", получаем данные формы
-    if (context.Request.Path == "/postuser")
+    if (path == "/postuser" && context.Request.Method == "POST")
     {
-        var form = context.Request.Form;
+        var form = await context.Request.ReadFormAsync();
         string name = form["name"];
         string age = form["age"];
-        await context.Response.WriteAsync($"<div><p>Name: {name}</p><p>Age: {age}</p></div>");
+        await response.WriteAsync($"<div><p>Name: {name}</p>"
+                                 + $"<p>Age: {age}</p></div>");
+    }
+    else if (path == "/image")
+    {
+        // Отправка изображения в ответ
+        response.ContentType = "image/jpeg";
+        await context.Response.SendFileAsync("image.jpg");
+    }
+    else if (path == "/download-image")
+    {
+        // Отправка изображения в ответ с заголовком Content-Disposition
+        response.Headers["Content-Disposition"] = "attachment; filename=my_image.jpg";
+        response.ContentType = "image/jpeg";
+        await context.Response.SendFileAsync("image.jpg");
+    }
+    else if (File.Exists($"html/{path}"))
+    {
+        // Отправка запрошенной HTML-страницы
+        response.ContentType = "text/html; charset=utf-8";
+        await response.SendFileAsync($"html/{path}");
     }
     else
     {
-        await context.Response.SendFileAsync("html/index.html");
+        response.ContentType = "text/html; charset=utf-8";
+
+        // Обработка ошибки 404
+        response.StatusCode = 404;
+        await response.WriteAsync("<h2>Not Found</h2>");
     }
 });
 
